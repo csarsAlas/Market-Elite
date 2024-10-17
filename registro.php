9c0a2755e1274545eeda2ffe61f7508a5d6c053e
csarsAlas <cesarsalas503@gmail.com>
2024-10-16 21:09:25 -0600

    registro.php

--- registro.php
+++ registro.php
@@ -12,63 +12,59 @@ $query = $selecciona->fetchAll(PDO::FETCH_ASSOC);
 
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport"content="width=device-width, initial-scale=1.0">
-        <title> Document</title>
-        <link rel="stylesheet" href="css/estilos-registo.css">
-    </head>
-    <body>
-        <header class="header">
-            <h1>ABARROTES FIME</h1>
-        </header>
-        <aside class="aside">
-            <ul>
-                <li><a href="index.html">Inicio</a></li>
-                <li><a href="productos.php">Agregar Producto</a></li>
-                <li><a href="clientes.php">Agregar Cliente</a></li>
-                <li><a href="proveedor.php">Agregar Proveedor</a></li>
-                <li><a href="venta.php">Generar ticket</a></li>
-                <li><a href="registro.php">Registro de ventas</a></li>
-            </ul>
-        </aside>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilos-registo.css">
+</head>
+<body>
+    <header class="header">
+        <h1>Market Elite</h1>
+    </header>
+    <aside class="aside">
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="productos.php">Agregar Producto</a></li>
+            <li><a href="clientes.php">Agregar Cliente</a></li>
+            <li><a href="proveedor.php">Agregar Proveedor</a></li>
+            <li><a href="venta.php">Generar Ticket</a></li>
+            <li><a href="registro.php">Registro de Ventas</a></li>
+        </ul>
+    </aside>
 
-<div class="container-table">
-    <table id="consulta" name="consulta">
-        <thead>
-            <tr>
-                <th style="width: 25%;">ID venta </th>
-                <th style="width: 25%;">Cantidad</th>
-                <th style="width: 25%;">Total</th>
-                <th style="width: 25%;">Fecha y hora </th>
-
-            </tr>
-        </thead>
-    <tbody>
-        <?php foreach ($query as $fila):
-
-        $cantidad = $cantidad + $fila['cantidad'];
-        $total=$total+$fila['total'];
-        ?>
-        <tr>
-            <td style="text-align: center;">
-            <?php echo $fila['id_venta']; ?>
-        </td>
-            <td style="text-align: center;">
-            <?php echo $fila['cantidad']; ?>
-        </td> 
-            <td style="text-align: center;">
-            <?php echo $fila['total']; ?>
-        </td> 
-            <td style="text-align: center;">
-            <?php echo $fila['fechaHora']; ?>
-        </td> 
-    </tr>
-        <?php endforeach;   ?>
-    </tbody>
-</table>
-    <h1>Total: $<?php echo $total ?></h1>
-    <h2>Cantidad de productos vendidos: <?php echo $cantidad ?></h2>
+    <div class="container-table">
+        <table id="consulta" name="consulta">
+            <thead>
+                <tr>
+                    <th>ID Venta</th>
+                    <th>Cantidad</th>
+                    <th>Total</th>
+                    <th>Fecha y Hora</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                $cantidad = 0; // Inicializar variables
+                $total = 0; 
+                foreach ($query as $fila): 
+                    $cantidad += $fila['cantidad'];
+                    $total += $fila['total'];
+                ?>
+                <tr>
+                    <td style="text-align: center;"><?php echo $fila['id_venta']; ?></td>
+                    <td style="text-align: center;"><?php echo $fila['cantidad']; ?></td>
+                    <td style="text-align: center;"><?php echo $fila['total']; ?></td>
+                    <td style="text-align: center;"><?php echo $fila['fechaHora']; ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <div class="totals">
+            <h1>Total: $<?php echo number_format($total, 2); ?></h1>
+            <h2>Cantidad de Productos Vendidos: <?php echo $cantidad; ?></h2>
+        </div>
+    </div>
 </body>
 </html>
